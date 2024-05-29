040652c5a2566241a56519ebd013dbda9e4a0f9c
Noel <dpunish3r@users.noreply.github.com>
2024-05-29 13:41:48 +0200

    Enable tx resend (#28)
    * enable tx resend

--- sequencesender/sequencesender.go
+++ sequencesender/sequencesender.go
@@ -414,8 +414,11 @@ func (s *SequenceSender) getResultAndUpdateEthTx(ctx context.Context, txHash com
 	if err == ethtxmanager.ErrNotFound {
 		log.Infof("[SeqSender] transaction %v does not exist in ethtxmanager. Marking it", txHash)
 		txData.OnMonitor = false
-		// Resend tx?
-		// _ = s.sendTx(ctx, true, &txHash, nil, 0, 0, nil)
+		// Resend tx
+		errSend := s.sendTx(ctx, true, &txHash, nil, 0, 0, nil)
+		if errSend == nil {
+			txData.OnMonitor = false
+		}
 	} else if err != nil {
 		log.Errorf("[SeqSender] error getting result for tx %v: %v", txHash, err)
 		return err
